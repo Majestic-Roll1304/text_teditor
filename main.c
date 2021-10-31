b4972a43cda382d14dfafe9152a90a6357dfe51b
Majestic-Roll1304 <raptorshinigami8@gmail.com>
2021-10-31 12:34:54 +0530

    breaking down into smaller chunks

--- main.c
+++ main.c
@@ -3,18 +3,9 @@
 #include<stdlib.h>
 #include<fcntl.h>
 #include<unistd.h>
-
+#include"pointer.c"
 //to change the location of the character pointer in the file
 //passing address to change the address of the original file descriptor
-int gotobegin(int *fd)
-{
-  return lseek(*fd,0,SEEK_SET);
-}
-
-int gotoend(int *fd)
-{
-  return lseek(*fd,0,SEEK_END);
-}
 
 int getfilesize(int *fd)
 {
